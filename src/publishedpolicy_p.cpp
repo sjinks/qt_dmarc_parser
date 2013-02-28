39a37096e7213603d15066293e6f849df89211b6
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2013-02-28 15:49:26 +0200

    More strict validation

--- src/publishedpolicy_p.cpp
+++ src/publishedpolicy_p.cpp
@@ -1,3 +1,4 @@
+#include <QtCore/QCoreApplication>
 #include <QtCore/QXmlStreamReader>
 #include "publishedpolicy_p.h"
 #include "private.h"
@@ -9,27 +10,114 @@ DMARC::PublishedPolicyPrivate::PublishedPolicyPrivate(void)
 
 bool DMARC::PublishedPolicyPrivate::parse(QXmlStreamReader& r)
 {
+	bool seen_domain = false;
+	bool seen_adkim  = false;
+	bool seen_aspf   = false;
+	bool seen_p      = false;
+	bool seen_sp     = false;
+	bool seen_pct    = false;
+
 	while (r.readNextStartElement()) {
 		if (r.name() == QLatin1String("domain")) {
-			this->domain = r.readElementText();
+			if (!seen_domain) {
+				seen_domain  = true;
+				this->domain = r.readElementText();
+			}
+			else {
+				r.raiseError(QCoreApplication::translate("dmarcparser", "Duplicate <%1> tag").arg(QLatin1String("domain")));
+			}
 		}
 		else if (r.name() == QLatin1String("adkim")) {
-			this->adkim = string2policy(r.readElementText());
+			if (!seen_adkim) {
+				seen_adkim    = true;
+				QString value = r.readElementText();
+				this->adkim   = string2policy(value);
+
+				if (DMARC::UnknownPolicy == this->adkim) {
+					r.raiseError(QCoreApplication::translate("dmarcparser", "Value '%1' is not valid for <%2>").arg(value, QLatin1String("adkim")));
+				}
+			}
+			else {
+				r.raiseError(QCoreApplication::translate("dmarcparser", "Duplicate <%1> tag").arg(QLatin1String("adkim")));
+			}
 		}
 		else if (r.name() == QLatin1String("aspf")) {
-			this->aspf = string2policy(r.readElementText());
+			if (!seen_aspf) {
+				seen_aspf     = true;
+				QString value = r.readElementText();
+				this->aspf    = string2policy(value);
+
+				if (DMARC::UnknownPolicy == this->aspf) {
+					r.raiseError(QCoreApplication::translate("dmarcparser", "Value '%1' is not valid for <%2>").arg(value, QLatin1String("aspf")));
+				}
+			}
+			else {
+				r.raiseError(QCoreApplication::translate("dmarcparser", "Duplicate <%1> tag").arg(QLatin1String("aspf")));
+			}
 		}
 		else if (r.name() == QLatin1String("p")) {
-			this->p = string2disposition(r.readElementText());
+			if (!seen_p) {
+				seen_p        = true;
+				QString value = r.readElementText();
+				this->p       = string2disposition(value);
+
+				if (DMARC::DispositionUnknown == this->p) {
+					r.raiseError(QCoreApplication::translate("dmarcparser", "Value '%1' is not valid for <%2>").arg(value, QLatin1String("p")));
+				}
+			}
+			else {
+				r.raiseError(QCoreApplication::translate("dmarcparser", "Duplicate <%1> tag").arg(QLatin1String("p")));
+			}
 		}
 		else if (r.name() == QLatin1String("sp")) {
-			this->sp = string2disposition(r.readElementText());
+			if (!seen_sp) {
+				seen_sp       = true;
+				QString value = r.readElementText();
+				this->sp      = string2disposition(value);
+
+				if (DMARC::DispositionUnknown == this->sp) {
+					r.raiseError(QCoreApplication::translate("dmarcparser", "Value '%1' is not valid for <%2>").arg(value, QLatin1String("sp")));
+				}
+			}
+			else {
+				r.raiseError(QCoreApplication::translate("dmarcparser", "Duplicate <%1> tag").arg(QLatin1String("sp")));
+			}
 		}
 		else if (r.name() == QLatin1String("pct")) {
-			this->pct = r.readElementText().toInt();
+			if (!seen_pct) {
+				bool ok;
+				seen_pct      = true;
+				QString value = r.readElementText();
+				this->pct     = value.toInt(&ok);
+
+				if (!ok) {
+					r.raiseError(QCoreApplication::translate("dmarcparser", "Value '%1' is not a valid integer").arg(value));
+				}
+			}
+			else {
+				r.raiseError(QCoreApplication::translate("dmarcparser", "Duplicate <%1> tag").arg(QLatin1String("pct")));
+			}
 		}
 		else {
-			r.raiseError(QLatin1String("Unexpected element"));
+			r.raiseError(QCoreApplication::translate("dmarcparser", "Unexpected element <%1>").arg(r.name().toString()));
+		}
+	}
+
+	if (!r.hasError()) {
+		if (!seen_domain) {
+			r.raiseError(QCoreApplication::translate("dmarcparser", "No <%1> tag").arg(QLatin1String("domain")));
+		}
+		else if (!seen_adkim) {
+			r.raiseError(QCoreApplication::translate("dmarcparser", "No <%1> tag").arg(QLatin1String("adkim")));
+		}
+		else if (!seen_aspf) {
+			r.raiseError(QCoreApplication::translate("dmarcparser", "No <%1> tag").arg(QLatin1String("aspf")));
+		}
+		else if (!seen_p) {
+			r.raiseError(QCoreApplication::translate("dmarcparser", "No <%1> tag").arg(QLatin1String("p")));
+		}
+		else if (!seen_pct) {
+			r.raiseError(QCoreApplication::translate("dmarcparser", "No <%1> tag").arg(QLatin1String("pct")));
 		}
 	}
 
