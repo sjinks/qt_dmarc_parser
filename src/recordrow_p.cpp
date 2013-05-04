6a3e96c6986dee273e69845d8f64aa74f1bcdae7
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2013-05-04 13:38:04 +0300

    Better handling of /feedback/record/row

--- src/recordrow_p.cpp
+++ src/recordrow_p.cpp
@@ -1,3 +1,4 @@
+#include <QtCore/QCoreApplication>
 #include <QtCore/QXmlStreamReader>
 #include "recordrow_p.h"
 #include "evaluatedpolicy.h"
@@ -10,19 +11,54 @@ DMARC::RecordRowPrivate::RecordRowPrivate(void)
 
 bool DMARC::RecordRowPrivate::parse(QXmlStreamReader& r)
 {
+	bool seen_sourceip = false;
+	bool seen_count    = false;
+
 	while (r.readNextStartElement()) {
 		if (r.name() == QLatin1String("source_ip")) {
-			this->source_ip = r.readElementText();
+			if (!seen_sourceip) {
+				seen_sourceip   = true;
+				this->source_ip = r.readElementText();
+			}
+			else {
+				r.raiseError(QCoreApplication::translate("dmarcparser", "Duplicate <%1> tag").arg(QLatin1String("source_ip")));
+			}
 		}
 		else if (r.name() == QLatin1String("count")) {
-			this->count = r.readElementText().toInt();
+			if (!seen_count) {
+				seen_count  = true;
+				bool ok     = false;
+				QString val = r.readElementText();
+				this->count = val.toInt(&ok);
+
+				if (!ok) {
+					r.raiseError(QCoreApplication::translate("dmarcparser", "Value '%1' is not a valid integer").arg(val));
+				}
+			}
+			else {
+				r.raiseError(QCoreApplication::translate("dmarcparser", "Duplicate <%1> tag").arg(QLatin1String("count")));
+			}
 		}
 		else if (r.name() == QLatin1String("policy_evaluated")) {
-			this->has_policy = true;
-			this->evaluated_policy.d_func()->parse(r);
+			if (!this->has_policy) {
+				this->has_policy = true;
+				this->evaluated_policy.d_func()->parse(r);
+			}
+			else {
+				r.raiseError(QCoreApplication::translate("dmarcparser", "Duplicate <%1> tag").arg(QLatin1String("policy_evaluated")));
+			}
 		}
 		else {
-			r.raiseError(QLatin1String("Unexpected element"));
+			r.raiseError(QCoreApplication::translate("dmarcparser", "Unexpected element <%1>").arg(r.name().toString()));
+		}
+	}
+
+	if (!r.hasError()) {
+		if (!seen_sourceip) {
+			r.raiseError(QCoreApplication::translate("dmarcparser", "No <%1> tag").arg(QLatin1String("source_ip")));
+		}
+		else if (!seen_count) {
+			r.raiseError(QCoreApplication::translate("dmarcparser", "No <%1> tag").arg(QLatin1String("count")));
 		}
 	}
 
