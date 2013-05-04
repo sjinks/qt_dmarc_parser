866ff515039e74154510a7b39a09dfb8994c190f
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2013-05-04 23:18:40 +0300

    Better handling of /feedback/record/auth_results/spf

--- src/spfauthenticationresult_p.cpp
+++ src/spfauthenticationresult_p.cpp
@@ -1,3 +1,4 @@
+#include <QtCore/QCoreApplication>
 #include <QtCore/QXmlStreamReader>
 #include "spfauthenticationresult_p.h"
 #include "private.h"
@@ -9,15 +10,43 @@ DMARC::SPFAuthenticationResultPrivate::SPFAuthenticationResultPrivate(void)
 
 bool DMARC::SPFAuthenticationResultPrivate::parse(QXmlStreamReader& r)
 {
+	bool seen_domain = false;
+	bool seen_result = false;
+
 	while (r.readNextStartElement()) {
 		if (r.name() == QLatin1String("domain")) {
-			this->domain = r.readElementText();
+			if (!seen_domain) {
+				seen_domain  = true;
+				this->domain = r.readElementText();
+			}
+			else {
+				r.raiseError(QCoreApplication::translate("dmarcparser", "Duplicate <%1> tag").arg(QLatin1String("domain")));
+			}
 		}
 		else if (r.name() == QLatin1String("result")) {
-			this->result = string2spfauthres(r.readElementText());
+			if (!seen_result) {
+				QString value = r.readElementText();
+				seen_result   = true;
+				this->result  = string2spfauthres(value);
+				if (DMARC::SpfUnknown == this->result) {
+					r.raiseError(QCoreApplication::translate("dmarcparser", "Value '%1' is not valid for <%2>").arg(value, QLatin1String("result")));
+				}
+			}
+			else {
+				r.raiseError(QCoreApplication::translate("dmarcparser", "Duplicate <%1> tag").arg(QLatin1String("result")));
+			}
 		}
 		else {
-			r.raiseError(QLatin1String("Unexpected element"));
+			r.raiseError(QCoreApplication::translate("dmarcparser", "Unexpected element <%1>").arg(r.name().toString()));
+		}
+	}
+
+	if (!r.hasError()) {
+		if (!seen_domain) {
+			r.raiseError(QCoreApplication::translate("dmarcparser", "No <%1> tag").arg(QLatin1String("domain")));
+		}
+		else if (!seen_result) {
+			r.raiseError(QCoreApplication::translate("dmarcparser", "No <%1> tag").arg(QLatin1String("result")));
 		}
 	}
 
