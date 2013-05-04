63af60a0d347465e2aa0d420f6a2a7a035c43970
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2013-05-04 22:54:59 +0300

    Better handling of /feedback/record/identifiers

--- src/recordidentifier_p.cpp
+++ src/recordidentifier_p.cpp
@@ -1,3 +1,4 @@
+#include <QtCore/QCoreApplication>
 #include <QtCore/QXmlStreamReader>
 #include "recordidentifier_p.h"
 
@@ -8,15 +9,36 @@ DMARC::RecordIdentifierPrivate::RecordIdentifierPrivate(void)
 
 bool DMARC::RecordIdentifierPrivate::parse(QXmlStreamReader& r)
 {
+	bool seen_envelope = false;
+	bool seen_from     = false;
+
 	while (r.readNextStartElement()) {
 		if (r.name() == QLatin1String("envelope_to")) {
-			this->envelope_to = r.readElementText();
+			if (!seen_envelope) {
+				seen_envelope     = true;
+				this->envelope_to = r.readElementText();
+			}
+			else {
+				r.raiseError(QCoreApplication::translate("dmarcparser", "Duplicate <%1> tag").arg(QLatin1String("envelope_to")));
+			}
 		}
 		else if (r.name() == QLatin1String("header_from")) {
-			this->header_from = r.readElementText();
+			if (!seen_from) {
+				seen_from         = true;
+				this->header_from = r.readElementText();
+			}
+			else {
+				r.raiseError(QCoreApplication::translate("dmarcparser", "Duplicate <%1> tag").arg(QLatin1String("header_from")));
+			}
 		}
 		else {
-			r.raiseError(QLatin1String("Unexpected element"));
+			r.raiseError(QCoreApplication::translate("dmarcparser", "Unexpected element <%1>").arg(r.name().toString()));
+		}
+	}
+
+	if (!r.hasError()) {
+		if (!seen_from) {
+			r.raiseError(QCoreApplication::translate("dmarcparser", "No <%1> tag").arg(QLatin1String("header_from")));
 		}
 	}
 
