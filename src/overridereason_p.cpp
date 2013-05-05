3766f170bbc6de4f9d40b38566e3b2456a75e2c2
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2013-05-05 13:48:57 +0300

    Better handling of /feedback/record/row/policy_evaluated/reason

--- src/overridereason_p.cpp
+++ src/overridereason_p.cpp
@@ -1,3 +1,4 @@
+#include <QtCore/QCoreApplication>
 #include <QtCore/QXmlStreamReader>
 #include "overridereason_p.h"
 #include "private.h"
@@ -9,15 +10,41 @@ DMARC::OverrideReasonPrivate::OverrideReasonPrivate(void)
 
 bool DMARC::OverrideReasonPrivate::parse(QXmlStreamReader& r)
 {
+	bool seen_type    = false;
+	bool seen_comment = false;
+
 	while (r.readNextStartElement()) {
 		if (r.name() == QLatin1String("type")) {
-			this->type = string2ot(r.readElementText());
+			if (!seen_type) {
+				QString val = r.readElementText();
+				seen_type   = true;
+				this->type  = string2ot(val);
+
+				if (DMARC::OverrideUnknown == this->type) {
+					r.raiseError(QCoreApplication::translate("dmarcparser", "Value '%1' is not valid for <%2>").arg(val, QLatin1String("type")));
+				}
+			}
+			else {
+				r.raiseError(QCoreApplication::translate("dmarcparser", "Duplicate <%1> tag").arg(QLatin1String("type")));
+			}
 		}
 		else if (r.name() == QLatin1String("comment")) {
-			this->comment = r.readElementText();
+			if (!seen_comment) {
+				seen_comment  = true;
+				this->comment = r.readElementText();
+			}
+			else {
+				r.raiseError(QCoreApplication::translate("dmarcparser", "Duplicate <%1> tag").arg(QLatin1String("comment")));
+			}
 		}
 		else {
-			r.raiseError(QLatin1String("Unexpected element"));
+			r.raiseError(QCoreApplication::translate("dmarcparser", "Unexpected element <%1>").arg(r.name().toString()));
+		}
+	}
+
+	if (!r.hasError()) {
+		if (!seen_type) {
+			r.raiseError(QCoreApplication::translate("dmarcparser", "No <%1> tag").arg(QLatin1String("type")));
 		}
 	}
 
