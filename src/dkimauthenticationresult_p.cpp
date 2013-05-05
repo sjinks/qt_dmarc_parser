918a96101c86ea2f466167ecb3ef9978a71eb1d1
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2013-05-05 13:59:31 +0300

    Better handling of /feedback/record/auth_results/dkim

--- src/dkimauthenticationresult_p.cpp
+++ src/dkimauthenticationresult_p.cpp
@@ -1,3 +1,4 @@
+#include <QtCore/QCoreApplication>
 #include <QtCore/QXmlStreamReader>
 #include "dkimauthenticationresult_p.h"
 #include "private.h"
@@ -9,18 +10,54 @@ DMARC::DKIMAuthenticationResultPrivate::DKIMAuthenticationResultPrivate(void)
 
 bool DMARC::DKIMAuthenticationResultPrivate::parse(QXmlStreamReader& r)
 {
+	bool seen_domain = false;
+	bool seen_result = false;
+	bool seen_human  = false;
+
 	while (r.readNextStartElement()) {
 		if (r.name() == QLatin1String("domain")) {
-			this->domain = r.readElementText();
+			if (!seen_domain) {
+				seen_domain  = true;
+				this->domain = r.readElementText();
+			}
+			else {
+				r.raiseError(QCoreApplication::translate("dmarcparser", "Duplicate <%1> tag").arg(QLatin1String("domain")));
+			}
 		}
 		else if (r.name() == QLatin1String("result")) {
-			this->result = string2dkimauthres(r.readElementText());
+			if (!seen_result) {
+				QString val  = r.readElementText();
+				seen_result  = true;
+				this->result = string2dkimauthres(val);
+
+				if (DMARC::DkimUnknown == this->result) {
+					r.raiseError(QCoreApplication::translate("dmarcparser", "Value '%1' is not valid for <%2>").arg(val, QLatin1String("result")));
+				}
+			}
+			else {
+				r.raiseError(QCoreApplication::translate("dmarcparser", "Duplicate <%1> tag").arg(QLatin1String("result")));
+			}
 		}
 		else if (r.name() == QLatin1String("human_result")) {
-			this->human_result = r.readElementText();
+			if (!seen_human) {
+				seen_human         = true;
+				this->human_result = r.readElementText();
+			}
+			else {
+				r.raiseError(QCoreApplication::translate("dmarcparser", "Duplicate <%1> tag").arg(QLatin1String("human_result")));
+			}
 		}
 		else {
-			r.raiseError(QLatin1String("Unexpected element"));
+			r.raiseError(QCoreApplication::translate("dmarcparser", "Unexpected element <%1>").arg(r.name().toString()));
+		}
+	}
+
+	if (!r.hasError()) {
+		if (!seen_domain) {
+			r.raiseError(QCoreApplication::translate("dmarcparser", "No <%1> tag").arg(QLatin1String("domain")));
+		}
+		else if (!seen_result) {
+			r.raiseError(QCoreApplication::translate("dmarcparser", "No <%1> tag").arg(QLatin1String("result")));
 		}
 	}
 
